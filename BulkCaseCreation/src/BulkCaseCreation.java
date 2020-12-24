b78692785addf304fafb1b770a5c175b1b756a5f
Administrator <Administrator@10.0.0.1>
2020-12-24 04:44:01 +0000

    LC

---
+++ BulkCaseCreation/src/BulkCaseCreation.java
@@ -0,0 +1,10 @@
+
+public class BulkCaseCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ActionClass obj = new ActionClass();
+		obj.fetchDocument();
+	}
+
+}
